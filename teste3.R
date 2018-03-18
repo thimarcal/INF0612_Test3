8039d5b04cbf8e4a3d9e80424fb7812360902259
Rodrigo Sim <rcsim10@gmail.com>
2018-03-18 15:55:13 -0300

    Added first exercise solution

--- teste3.R
+++ teste3.R
@@ -14,7 +14,30 @@ gcd2 <- function(x, y) {
   }
 }
 
+gcd <- function(...) {
 
+  values <- c(...)
+  valuesLen <- length(values)
+
+  #if more than 1 value, calc base mdc with
+  #first 2 values
+  if(valuesLen > 1 ){
+    mdc <- gcd2(values[1], values[2])
+  }
+  #if only 1 value, return itself
+  else{
+    return(values[1])
+  }
+
+  #If length => 3, calc mdc for each value with
+  # base mdc and update base mdc
+  i <- 3
+  while(i <= valuesLen) {
+    mdc <- gcd2(values[i], mdc)
+    i <- i + 1
+  }
+  return(mdc)
+}
 
 
 
